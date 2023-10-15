00fb5809cd8c04ecc92af69d45963ab5a13fe780
l2k <ekyi0421@gmail.com>
2023-10-15 12:53:10 +0900

    feat: MainApplication 추가

---
+++ app-demo-compose/src/main/java/com/nyangzzi/response_layout_grid/app_demo_compose/MainApplication.kt
@@ -0,0 +1,7 @@
+package com.nyangzzi.response_layout_grid.app_demo_compose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application()
