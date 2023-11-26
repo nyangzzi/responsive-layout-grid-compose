f57f4e3592f387aad905d8a8283611689897fc4b
l2k <ekyi0421@gmail.com>
2023-11-26 22:37:29 +0900

    feat: app demo ResponsiveColumnSample 추가

--- app-demo/src/main/java/com/nyangzzi/responsive_layout_grid/app_demo/MainActivity.kt
+++ app-demo/src/main/java/com/nyangzzi/responsive_layout_grid/app_demo/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
@@ -19,6 +25,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nyangzzi.responsive_layout_grid.app_demo.ui.theme.ResponsiveLayoutGridComposeTheme
 import com.nyangzzi.responsive_layout_grid_compose.core.ResponsiveConfig
+import com.nyangzzi.responsive_layout_grid_compose.core.column.ResponsiveColumn
 import com.nyangzzi.responsive_layout_grid_compose.core.row.ResponsiveRow
 import com.nyangzzi.responsive_layout_grid_compose.core.row.ResponsiveBreakPoint
 import dagger.hilt.android.AndroidEntryPoint
@@ -34,8 +41,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    //ResponsiveColumnSample()
                     ResponsiveRowSample()
                 }
+            }
+        }
+    }
+}
+
+@Composable
+fun ResponsiveColumnSample(){
+    Row(modifier = Modifier
+        .fillMaxSize()
+        .horizontalScroll(rememberScrollState()),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+
+        val totalRows = remember {
+            mutableStateOf(5)
+        }
+
+        for (index in 1 until totalRows.value) {
+            ResponsiveColumn(config = ResponsiveConfig.set(gutter = 16.dp, vertical = 8.dp),
+                totalRows = totalRows.value) {
+                Text(modifier = Modifier
+                    .verticalWeight(index)
+                    .background(Color.LightGray)
+                    .padding(4.dp), text = "$index")
+                Text(modifier = Modifier
+                    .verticalWeight(totalRows.value - index)
+                    .fillMaxSize()
+                    .background(Color.LightGray)
+                    .padding(4.dp), text = "${totalRows.value-index}")
             }
         }
     }
@@ -45,19 +82,25 @@ class MainActivity : ComponentActivity() {
 fun ResponsiveRowSample() {
     Column(modifier = Modifier
         .fillMaxSize()
-        .verticalScroll(rememberScrollState())) {
+        .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
 
         val layoutWidth = LocalConfiguration.current.screenWidthDp.dp
         val screenType = ResponsiveBreakPoint.getScreenType(layoutWidth)
 
         for (index in 1 until screenType.noOfColumns) {
-            ResponsiveRow(config = ResponsiveConfig.set(gutter = 20.dp, vertical = 20.dp)) {
+            ResponsiveRow(config = ResponsiveConfig.set(gutter = 16.dp, horizontal = 8.dp),
+                totalColumns = screenType.noOfColumns) {
                 Text(modifier = Modifier
                     .horizontalWeight(index)
-                    .background(Color.LightGray), text = "$index")
+                    .background(Color.LightGray)
+                    .padding(4.dp), text = "$index")
                 Text(modifier = Modifier
                     .horizontalWeight(screenType.noOfColumns - index)
-                    .background(Color.LightGray), text = "${screenType.noOfColumns-index}")
+                    .fillMaxSize()
+                    .background(Color.LightGray)
+                    .padding(4.dp), text = "${screenType.noOfColumns-index}")
             }
         }
     }
