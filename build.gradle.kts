e50663f062ff1645646c317a26c8fbb1475170bc
l2k <ekyi0421@gmail.com>
2024-08-11 02:35:02 +0900

    build: add artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,3 @@
-buildscript {
-    dependencies {
-        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
-        //classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.7.10")
-    }
-}
-
 allprojects {
     group = "com.github.nyangzzi"
 }
@@ -14,4 +7,18 @@ plugins {
     id (Plugins.ANDROID_LIBRARY) version Versions.AGP apply false
     id (Plugins.KOTLIN_ANDROID) version Versions.KOTLIN apply false
     id("org.jetbrains.dokka") version "1.7.10"
+    id("maven-publish") // Add maven-publish plugin
+    java
+    `java-library`
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven-public") {
+            groupId = "com.msg"
+            artifactId = "library"
+            version = "1.0.0"
+            from(components["java"])
+        }
+    }
 }
